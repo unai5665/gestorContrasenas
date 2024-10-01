54f4936a5d1c317e2afcf6d622b7b8ab4d980547
unai <unaiperez5665@gmail.com>
2024-10-01 13:19:56 +0100

    Actualizado el color del fondo

--- app/src/main/java/com/example/gestorcontrasenas/MainActivity.kt
+++ app/src/main/java/com/example/gestorcontrasenas/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.gestorcontrasenas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.gestorcontrasenas.ui.theme.GestorContrasenasTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +25,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GestorContrasenasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.White
+                ){
+                    GreetingText()
                 }
             }
         }
@@ -30,17 +37,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun GreetingText(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
-}
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .padding(end = 16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GestorContrasenasTheme {
-        Greeting("Android")
+        )
     }
 }
+
