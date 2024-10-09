d9a1f84e7ccb10d750bcc1ba48855d60f29322de
Iriemst <151166883+Iriemst@users.noreply.github.com>
2024-10-09 10:05:02 +0100

    Update MainActivity.kt
    creamos una clase de datos, la vamos a usar para guardar varios valores como una "variable"

--- app/src/main/java/com/example/gestorcontrasenas/MainActivity.kt
+++ app/src/main/java/com/example/gestorcontrasenas/MainActivity.kt
@@ -27,6 +27,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.gestordecontraseas.ui.theme.GestorDeContrasenasTheme
 
+
+data class EntradaContraseña(val id: Int, val plataforma: String, val usuario: String, val contraseña: String)
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
